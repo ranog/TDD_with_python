35eb157b72c3d9e0749a0f16647b4e4bddaf64a0
ranog <jprnogueira@yahoo.com.br>
2021-07-09 18:29:47 -0300

    added the second parameter in assert keyword

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'To-Do' in browser.title
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
 
 browser.quit()
