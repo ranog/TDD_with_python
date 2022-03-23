44dc6f510e4b56c53a888d37f67dc8b4c2eed84b
ranog <jprnogueira@yahoo.com.br>
2022-03-22 21:32:15 -0300

    Processing a POST Request on the Server

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,3 +6,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response=response, template_name='home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn(member='A new list item', container=response.content.decode())
