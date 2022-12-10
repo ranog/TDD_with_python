d30d35a2e4aa5f79d56695087debb7070df62af4
ranog <jprnogueira@yahoo.com.br>
2022-12-09 23:33:15 -0300

    beginning of studies

--- superlists/lists/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def home_page(request):
-    return render(
-        request=request,
-        template_name='home.html',
-        context={'new_item_text': request.POST.get('item_text', '')},
-    )
