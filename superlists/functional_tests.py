2f7ff0408b8cc69918ede66c52b6497541ab91f9
ranog <jprnogueira@yahoo.com.br>
2022-12-11 21:11:04 -0300

    first functional test uses unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,16 @@
+from unittest import TestCase
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Congratulations!' in browser.title
+class NewVisitorTest(TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!!!')
